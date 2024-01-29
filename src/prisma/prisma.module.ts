7abaa6a330acbb606c4bb4fdb1e9032de23f9fac
unknown <amirreza.asari2@gmail.com>
2024-01-29 05:34:06 +0330

    user module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() //since we are using it all over the project
 @Module({
   providers: [PrismaService],
   exports : [PrismaService] //we need to export it so we can use it in all services
